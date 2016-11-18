6011567cb8ab4866b18db3c2580755a32016a6f5
Chi-En Wu <jason2506.mail@gmail.com>
2016-11-18 20:39:26 +0800

    Add class `transform_iterator<>`

---
+++ include/literator/internal/transform_iterator_base.hpp
@@ -0,0 +1,62 @@
+// (c) Copyright David Abrahams 2002.
+// (c) Copyright Jeremy Siek 2002.
+// (c) Copyright Thomas Witt 2002.
+// (c) Copyright Chi-En Wu 2016.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef LITERATOR_INTERNAL_TRANSFORM_ITERATOR_BASE_HPP_
+#define LITERATOR_INTERNAL_TRANSFORM_ITERATOR_BASE_HPP_
+
+#include <iterator>
+#include <type_traits>
+
+#include "use_default.hpp"
+
+#include "../iterator_adaptor.hpp"
+
+namespace literator {
+
+// forward declaration
+template <typename Transform, typename Iterator, typename Reference, typename Value>
+class transform_iterator;
+
+namespace internal {
+
+/************************************************
+ * Declaration: class transform_iterator_reference<F, I, R, V>
+ ************************************************/
+
+template <typename Transform, typename Iterator, typename Reference, typename Value>
+using transform_iterator_reference = typename get_with_default<
+    Reference,
+    typename std::result_of<
+        const Transform(typename std::iterator_traits<Iterator>::reference)
+    >::type
+>::type;
+
+/************************************************
+ * Declaration: class transform_iterator_base<F, I, R, V>
+ ************************************************/
+
+template <typename Transform, typename Iterator, typename Reference, typename Value>
+using transform_iterator_base = iterator_adaptor<
+    transform_iterator<Transform, Iterator, Reference, Value>,
+    Iterator,
+    typename get_with_default<
+        Value,
+        std::remove_reference<
+            transform_iterator_reference<Transform, Iterator, Reference, Value>
+        >
+    >::type,
+    use_default,
+    transform_iterator_reference<Transform, Iterator, Reference, Value>
+>;
+
+}  // namespace internal
+
+}  // namespace literator
+
+#endif  // LITERATOR_INTERNAL_TRANSFORM_ITERATOR_BASE_HPP_
---
+++ include/literator/transform_iterator.hpp
@@ -0,0 +1,63 @@
+// (c) Copyright David Abrahams 2002.
+// (c) Copyright Jeremy Siek 2002.
+// (c) Copyright Thomas Witt 2002.
+// (c) Copyright Chi-En Wu 2016.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef LITERATOR_TRANSFORM_ITERATOR_HPP_
+#define LITERATOR_TRANSFORM_ITERATOR_HPP_
+
+#include "internal/transform_iterator_base.hpp"
+
+namespace literator {
+
+/************************************************
+ * Declaration: class transform_iterator<F, I, R, V>
+ ************************************************/
+
+template <
+    typename Transform,
+    typename Iterator,
+    typename Reference = use_default,
+    typename Value = use_default
+>
+class transform_iterator : public internal::transform_iterator_base<
+    Transform, Iterator, Reference, Value
+> {
+    friend class iterator_core_access;
+
+ private:  // Private Type(s)
+    using super_t = typename internal::transform_iterator_base<
+        Transform, Iterator, Reference, Value
+    >;
+
+ public:  // Public Methods(s)
+    transform_iterator() = default;
+    transform_iterator(Transform f, Iterator it)
+        : super_t(it), f_(f) { /* do nothing */ }
+
+ private:  // Private Method(s)
+    typename super_t::reference dereference() const {
+        return f_(*this->base());
+    }
+
+ private:  // Private Properties(s)
+    Transform f_;
+};  // class transform_iterator<F, I, R, V>
+
+/************************************************
+ * Declaration: Non-member Function(s)
+ ************************************************/
+
+template <typename Transform, typename Iterator>
+inline transform_iterator<Transform, Iterator>
+make_transform_iterator(Transform f, Iterator it) {
+    return transform_iterator<Transform, Iterator>(f, it);
+}
+
+}  // namespace literator
+
+#endif  // LITERATOR_TRANSFORM_ITERATOR_HPP_
