73ce8a4df3731e3976c18c11286e20229223d239
Chi-En Wu <jason2506.mail@gmail.com>
2016-12-07 19:20:03 +0800

    Refine `conanfile.py`

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conans import ConanFile, CMake
 
 
-class DesaConan(ConanFile):
+class LiteratorConan(ConanFile):
 
     name = 'literator'
     version = '0.1.0'
@@ -9,8 +9,8 @@ class DesaConan(ConanFile):
     license = 'BSL-1.0'
     author = 'Chi-En Wu'
 
-    settings = 'os', 'compiler', 'build_type', 'arch'
-    generators = 'cmake'
+    settings = ('os', 'compiler', 'build_type', 'arch')
+    generators = ('cmake', 'txt', 'env')
 
     exports = (
         'CMakeLists.txt',
@@ -19,12 +19,15 @@ class DesaConan(ConanFile):
     )
 
     def build(self):
+        extra_opts = []
+
         cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (
-            self.conanfile_directory,
-            cmake.command_line
+        self.run('cmake "{src_dir}" {opts} {extra_opts}'.format(
+            src_dir=self.conanfile_directory,
+            opts=cmake.command_line,
+            extra_opts=' '.join(extra_opts),
         ))
+        self.run('cmake --build . {}'.format(cmake.build_config))
 
     def package(self):
-        self.copy('*.cmake', dst='', src='cmake')
-        self.copy('*.hpp', dst='include', src='include')
+        self.run('cmake --build . --target install')
